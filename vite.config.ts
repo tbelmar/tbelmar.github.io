357688aae8f599f2393164f1ae3d90720e6f5fe0
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-07 00:36:22 +0100

    use process.env.PORT

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 3000,
+		port: process.env.PORT || 3000,
 		strictPort: false
 	}
 });
