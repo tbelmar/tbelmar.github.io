c109acb8d62df1617370b8fbc00bc5ec7eeecb41
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-07 00:00:04 +0100

    added interactive animations to my character

---
+++ src/lib/helpers.ts
@@ -0,0 +1,23 @@
+import {Texture, AnimatedSprite} from 'pixi.js';
+
+export async function loadAnimatedSprite(
+    name: String,
+    framesAmount = Infinity
+) {
+    let frames = [];
+
+    try {
+        for (let i = 1; i <= framesAmount; i++) {
+            let texture = await Texture.fromURL(
+                `animations/${name}/${name}${i}.png`
+            );
+            frames.push(texture);
+        }
+    } catch (err) {
+        console.log('Error: File does not exist');
+    }
+
+    let sprite = new AnimatedSprite(frames);
+
+    return sprite;
+}
