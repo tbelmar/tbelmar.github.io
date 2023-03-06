7ec4c640eadce843590961399be1fd90fbf9c856
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-06 18:34:32 +0100

    set up svelte project. ready to work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Header from "../lib/Header.svelte";
+  import { Application, Assets, Sprite, Renderer } from "pixi.js";
+
+  onMount(() => {
+    const app = new Application({
+      width: 100,
+      height: 100,
+    });
+
+    console.log(app);
+  });
+</script>
