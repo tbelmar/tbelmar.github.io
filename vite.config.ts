a565eaecb246ef216501782b6fc3e9796f4f3deb
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-07 06:23:52 +0100

    added debug server log

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { loadEnv } from 'vite';
 import { defineConfig } from 'vite';
 
+console.log("THE PORT IS " + process.env.PORT)
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
