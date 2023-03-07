216b57f5bba88cde93a3f6d75dab977292700714
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-07 07:04:09 +0100

    Revert "added debug server log"
    This reverts commit a565eaecb246ef216501782b6fc3e9796f4f3deb.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { loadEnv } from 'vite';
 import { defineConfig } from 'vite';
 
-console.log("THE PORT IS " + process.env.PORT)
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
