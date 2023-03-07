b75bca1e681f9373941296a4ea278d0f800e5303
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-03-07 07:15:36 +0100

    added procfile

---
+++ build/server/chunks/2-cc293286.js
@@ -0,0 +1,9 @@
+const index = 2;
+const component = async () => (await import('./_page.svelte-1e963fe9.js')).default;
+const file = '_app/immutable/entry/_page.svelte.24f2620f.js';
+const imports = ["_app/immutable/entry/_page.svelte.24f2620f.js","_app/immutable/chunks/index.f98f3a93.js"];
+const stylesheets = ["_app/immutable/assets/_page.c46e5d35.css"];
+const fonts = [];
+
+export { component, file, fonts, imports, index, stylesheets };
+//# sourceMappingURL=2-cc293286.js.map
---
+++ build/server/chunks/_page.svelte-1e963fe9.js
@@ -0,0 +1,23 @@
+import { c as create_ssr_component, v as validate_component, d as add_attribute } from './index-3ce6e6d7.js';
+import 'pixi.js';
+
+const css = {
+  code: "@font-face{font-family:edunline;src:url('https://fonts.cdnfonts.com/s/7348/edunline.woff')}@font-face{font-family:o4b;src:url('https://fonts.cdnfonts.com/s/7340/04B_03__.woff')}.info-container.svelte-13rugg0{display:inline-block;position:absolute;left:50%;transform:translate(-50%, 0);width:100%;text-align:center;user-select:none;margin-top:10px;font-family:edunline;text-align:center}.name.svelte-13rugg0{color:#d8d9ba;text-shadow:-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,\n            5px 5px 0 #000;font-size:55px;margin-top:10px;margin-bottom:-10px;line-height:55px;letter-spacing:3px}.job-title.svelte-13rugg0{display:inline-block;font-family:o4b;color:#d8d9ba;text-shadow:-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,\n            1px 1px 0 #000;font-size:20px;letter-spacing:1px;border-style:solid;border-color:#351c24;background-color:rgb(141, 74, 60, 1);border-radius:10px;padding:5px 10px 3px 10px;box-shadow:2px 2px #000}",
+  map: null
+};
+const Header = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  $$result.css.add(css);
+  return `<div class="${"info-container svelte-13rugg0"}"><h1 class="${"name svelte-13rugg0"}">TOMÁS BELMAR</h1>
+    <p class="${"job-title svelte-13rugg0"}">Software Developer</p>
+</div>`;
+});
+const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  let canvas;
+  return `<div>${validate_component(Header, "Header").$$render($$result, {}, {}, {})}
+
+    <div class="${"app"}"><canvas${add_attribute("this", canvas, 0)}></canvas></div>
+</div>`;
+});
+
+export { Page as default };
+//# sourceMappingURL=_page.svelte-1e963fe9.js.map
